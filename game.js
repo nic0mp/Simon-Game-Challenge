02825f4c39d1528fd79093baf4b2c231ea57f80e
nic0mp <nicopinguel@gmail.com>
2021-09-14 20:48:27 -0500

    R1

--- game.js
+++ game.js
@@ -1,3 +1,10 @@
+
+buttonColours = ["red", "blue", "green", "yellow"];
+
+gamePattern = [];
+
 function nextSequence() {
-  var randomNumber =  (Math.random() * 4)
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomChosenColour];
+  gamePattern.push(randomChosenColour);
 };
